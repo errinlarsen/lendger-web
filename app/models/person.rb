d6e477a275f807c58e4f1d028a82062075f529a7
Errin Larsen <errinlarsen@gmail.com>
2013-11-11 18:55:54 -0600

    Add #lend and #borrow to Person
    The :loans attribute belongs to Person, and Person should manage its own Loans.

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,7 +7,17 @@ class Person
   attribute :id, Integer
   attribute :name, String
   attribute :email, String
-  attribute :loans, Array['Loan']
-end
+  attribute :loans, Array["Loan"]
 
-Virtus.finalize  # Finalize constants manually to resolve circular dependencies
+  def lend(thing, borrower)
+    new_loan = Loan.new(lender: self, thing: thing, borrower: borrower)
+    loans << new_loan
+    return new_loan
+  end
+
+  def borrow(thing, lender)
+    new_loan = Loan.new(lender: lender, thing: thing, borrower: self)
+    loans << new_loan
+    return new_loan
+  end
+end
--- spec/unit/person_spec.rb
+++ spec/unit/person_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + "/unit_helper")
+require_relative "unit_helper"
 require "models/person"
 
 describe Person do
@@ -24,6 +24,43 @@ describe Person do
     it "includes a loans collection" do
       expect(person.attributes).to include(:loans)
       expect(person.attributes[:loans]).to respond_to(:each)
+    end
+  end
+
+  describe "#lend(thing, borrower)" do
+    let(:thing) { {name: "foo"} }
+    let(:borrower) { {name: "bar"} }
+    let(:new_loan) { person.lend(thing, borrower) }
+    let(:expected_loan_attributes) do
+      {lender: person, thing: thing, borrower: borrower}
+    end
+
+    it "adds a new loan to its loans collection" do
+      expect(person.loans).to include(new_loan)
+    end
+    it "returns a new loan with itself as the :lender" do
+      expect(new_loan.lender).to eql(person)
+    end
+    it "returns a new loan with the correctl associations" do
+      expect(new_loan.thing[:name]).to eql(thing[:name])
+      expect(new_loan.borrower[:name]).to eql(borrower[:name])
+    end
+  end
+
+  describe "#borrow(thing, lender)" do
+    let(:thing) { {name: "foo"} }
+    let(:lender) { {name: "bar"} }
+    let(:new_loan) { person.borrow(thing, lender) }
+
+    it "adds a new loan to its loans collection" do
+      expect(person.loans).to include(new_loan)
+    end
+    it "returns a new loan with itself as the :borrower" do
+      expect(new_loan.borrower).to eql(person)
+    end
+    it "returns a new loan with the correct associations" do
+      expect(new_loan.thing[:name]).to eql(thing[:name])
+      expect(new_loan.lender[:name]).to eql(lender[:name])
     end
   end
 end
